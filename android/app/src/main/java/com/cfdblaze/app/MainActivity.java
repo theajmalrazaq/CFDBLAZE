f41760c895a66a96c5c379449d22f5adfe45d132
Ajmal Razaq Bhatti <theajmalrazaq@gmail.com>
2025-08-25 01:43:13 +0500

    removed

--- android/app/src/main/java/com/cfdblaze/app/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package com.cfdblaze.app;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
--- src/App.jsx
+++
@@ -1,9 +0,0 @@
-import Onboarding from './pages/onboarding'
-function App() {
-  return (<>
-    <Onboarding />
-  </>
-  )
-}
-
-export default App
